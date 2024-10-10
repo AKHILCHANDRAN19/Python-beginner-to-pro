23b2a2d6cf70f4c63515818bd87a871f56cc0133
AKHILCHANDRAN19 <hdcbmktr20@gmail.com>
2024-10-10 17:47:21 +0530

    Create Band name.py

---
+++ Day1/Print function/Band name.py
@@ -0,0 +1,4 @@
+print('Welcome to the Band name Generator')
+city = input("What's the name of the city you grew up?\n")
+pet = input("what's your pet name?\n")
+print("Your brand name could be "  +  city + " " + pet)

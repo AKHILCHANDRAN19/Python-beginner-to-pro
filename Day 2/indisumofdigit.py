ee0f70b78e849c342e55aa55ca6eff0267d575ad
AKHILCHANDRAN19 <hdcbmktr20@gmail.com>
2024-10-10 18:56:53 +0530

    Update indisumofdigit.py

--- Day 2/indisumofdigit.py
+++ Day 2/indisumofdigit.py
@@ -21,3 +21,14 @@ first_digit = int(number[0])
 second_digit = int(number[1])
 total = str(first_digit + second_digit)
 print("The sum total of two digit number you entered is " + total)
+
+
+
+#OR
+
+
+number = input("write down your number ")
+first_digit = int(number[0])
+second_digit = int(number[1])
+total = str(first_digit + second_digit)
+print("The sum total of two digit number  "  + number  + ' is ' + total)

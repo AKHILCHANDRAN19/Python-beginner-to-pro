8b645b0fbc4131ae134dcf8516ec0b8f2dae2b54
AKHILCHANDRAN19 <hdcbmktr20@gmail.com>
2024-10-10 18:49:04 +0530

    Update indisumofdigit.py

--- Day 2/indisumofdigit.py
+++ Day 2/indisumofdigit.py
@@ -3,3 +3,12 @@ first_digit = number[0]
 second_digit = number[1]
 total = int(first_digit) + int(second_digit)
 print(total)
+
+
+#OR
+
+number = input("write down your number ")
+first_digit = int(number[0])
+second_digit = int(number[1])
+total = first_digit + second_digit
+print(total)

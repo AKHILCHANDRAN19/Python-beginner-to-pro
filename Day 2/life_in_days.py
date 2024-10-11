1bfdb5e74bb09ee39fda5f1902c981d1aa61ea18
AKHILCHANDRAN19 <hdcbmktr20@gmail.com>
2024-10-11 12:33:11 +0530

    Update life_in_days.py

--- Day 2/life_in_days.py
+++ Day 2/life_in_days.py
@@ -3,3 +3,15 @@ remaining_days = 32850 - (365 * present_age)
 remaining_weeks = 4680 - (52 * present_age)
 remaining_months = 1080 - (12 * present_age)
 print(f"you have {remaining_days} days or you have {remaining_weeks} weeks or you have {remaining_months} months")
+
+
+
+#little advanced
+
+
+present_age = int(input('What is your current age?\n'))
+destination_age = int(input('what is your dream age you want to live?\n'))
+remaining_days = (destination_age * 365) - (365 * present_age)
+remaining_weeks = (destination_age * 52) - (52 * present_age)
+remaining_months = (destination_age * 12) - (12 * present_age)
+print(f"you have {remaining_days} days or you have {remaining_weeks} weeks or you have {remaining_months} months 🥰")

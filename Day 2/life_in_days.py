05d6df4e1c64023aab1f29d91c895df0c61ff20c
AKHILCHANDRAN19 <hdcbmktr20@gmail.com>
2024-10-11 12:23:39 +0530

    Create life_in_days.py

---
+++ Day 2/life_in_days.py
@@ -0,0 +1,5 @@
+present_age = int(input('What is your current age?\n'))
+remaining_days = 32850 - (365 * present_age)
+remaining_weeks = 4680 - (52 * present_age)
+remaining_months = 1080 - (12 * present_age)
+print(f"you have {remaining_days} days or you have {remaining_weeks} weeks or you have {remaining_months} months")

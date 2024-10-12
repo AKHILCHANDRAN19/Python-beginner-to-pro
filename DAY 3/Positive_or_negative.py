72ecb35e2326523e44bcc0e3015b09ea1bedfec6
AKHILCHANDRAN19 <hdcbmktr20@gmail.com>
2024-10-12 10:48:21 +0530

    Update Positive_or_negative.py

--- DAY 3/Positive_or_negative.py
+++ DAY 3/Positive_or_negative.py
@@ -5,3 +5,18 @@ elif number == 0:
     print('The provided number is zero')
 else:
 	print("The provided number is a positive number")
+
+
+
+#OR
+
+
+num = float(input("Enter a number: "))
+
+if num > 0:
+    print(f"{num} is a positive number.")
+elif num < 0:
+    print(f"{num} is a negative number.")
+else:
+    print(f"{num} is zero.")
+

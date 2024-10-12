27729ed0e3a00877a1dff068963c456a6c5680ce
AKHILCHANDRAN19 <hdcbmktr20@gmail.com>
2024-10-12 10:47:26 +0530

    Create Positive_or_negative.py

---
+++ DAY 3/Positive_or_negative.py
@@ -0,0 +1,7 @@
+number = float(input("Write your input number\n"))
+if number < 0:
+	print("The provided number is a negative number")
+elif number == 0:
+    print('The provided number is zero')
+else:
+	print("The provided number is a positive number")

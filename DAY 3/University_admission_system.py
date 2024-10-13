c9f33afb48ced2186647783e84f071c7d275c21d
AKHILCHANDRAN19 <hdcbmktr20@gmail.com>
2024-10-13 13:27:27 +0530

    Create University_admission_system.py

---
+++ DAY 3/University_admission_system.py
@@ -0,0 +1,7 @@
+#University Admission System
+#Question: A university has different criteria for admitting students based on their scores in Math, Science, and English:
+
+#The student must have an average score of at least 70 to be considered.
+#If the average score is between 70 and 85, the student is admitted if they have scored at least 75 in Math.
+#If the average score is above 85, they are admitted directly unless they have scored below 60 in any subject.
+#If a student scores above 90 in all three subjects, they receive a scholarship.

af2e142851473062fbfebf7933fa84a7b1d3f577
AKHILCHANDRAN19 <hdcbmktr20@gmail.com>
2024-10-16 08:20:06 +0530

    Update random_module_head_tail.py

--- DAY 4/random_module_head_tail.py
+++ DAY 4/random_module_head_tail.py
@@ -4,3 +4,13 @@ if integer == 1:
   print('head')
 elif integer == 2:
   print('tail')
+
+
+# OR
+
+import random
+integer = str(random.randint(1,2))
+if integer == '1':
+  print('head')
+elif integer == '2':
+  print('tail')

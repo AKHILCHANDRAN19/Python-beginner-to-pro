cf368592ae4050c0ab6770cd880ad03ef7b9a572
AKHILCHANDRAN19 <hdcbmktr20@gmail.com>
2024-10-19 08:36:43 +0530

    Update random_module_head_tail.py

--- DAY 4/random_module_head_tail.py
+++ DAY 4/random_module_head_tail.py
@@ -14,3 +14,14 @@ if integer == '1':
   print('head')
 elif integer == '2':
   print('tail')
+
+
+#OR
+
+import random
+result = random.randint(1,10000)
+new_result = result % 2
+if new_result == 0:
+  print(f'your random number is {result} so your option is head')
+else:
+  print(f'your random number is {result} so your option is tail')

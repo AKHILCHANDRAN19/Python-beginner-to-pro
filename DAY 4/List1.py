e9567560dd355e3e9ca08ee84bf8b80ce5587ad4
AKHILCHANDRAN19 <hdcbmktr20@gmail.com>
2024-11-07 09:28:20 +0530

    Create List1.py

---
+++ DAY 4/List1.py
@@ -0,0 +1,7 @@
+#Indian states and calling data from a particular position
+
+states_of_india = ["Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Karnataka", "Uttar Pradesh"]
+print(states_of_india[0])
+print(states_of_india[-1])
+print(states_of_india[1])
+print(states_of_india[-2])

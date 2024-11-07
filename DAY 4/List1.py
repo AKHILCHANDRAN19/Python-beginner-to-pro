cb41cdeb292a760ccfdcdb69b6aefb6a555e98fa
AKHILCHANDRAN19 <hdcbmktr20@gmail.com>
2024-11-07 09:34:02 +0530

    Update List1.py

--- DAY 4/List1.py
+++ DAY 4/List1.py
@@ -5,3 +5,9 @@ print(states_of_india[0])
 print(states_of_india[-1])
 print(states_of_india[1])
 print(states_of_india[-2])
+
+#replacing a listed item by changing it's spelling
+
+states_of_india = ["Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Karnataka", "Uttar Pradesh"]
+states_of_india[0] = "Anthra Pradesh"
+print(states_of_india)

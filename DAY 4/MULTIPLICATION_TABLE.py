056613ff4e972e233d0fcd8be38f1b2bdf2d34d2
AKHILCHANDRAN19 <hdcbmktr20@gmail.com>
2024-11-24 10:02:24 +0530

    Create MULTIPLICATION_TABLE.py

---
+++ DAY 4/MULTIPLICATION_TABLE.py
@@ -0,0 +1,5 @@
+print("WELCOME TO MULTIPLICATION TABLE ➗️➕️➖️")
+number = int(input("Enter your number\n"))
+for i in range(1, 11):
+  new_number = number * i
+  print(f"{number} * {i} = {new_number}")
